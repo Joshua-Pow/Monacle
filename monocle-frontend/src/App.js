01367d4832fe02f8231ae09b9923d1e2c3a2f686
jongjin-j <jjjung3571@gmail.com>
2021-11-06 14:12:46 -0400

    Input for URL and pdf

--- monocle-frontend/src/App.js
+++ monocle-frontend/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { TextField, Input, Button } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className="header">
+          <h2>Monocle</h2>
+        </div>
+        <div className="url-mode">
+          <TextField 
+            id="outlined-basic" 
+            label="URL" 
+            variant="outlined" 
+            placeholder="Enter a URL"
+          />
+        </div>
+        <div className="or">
+          OR
+        </div>
+        <div className="pdf-mode">
+        <label htmlFor="contained-button-file">
+          <Input accept="image/*" id="contained-button-file" multiple type="file" />
+          <Button variant="contained" component="span">
+            Upload
+          </Button>
+        </label>
+        </div>
     </div>
   );
 }
