0ce60fd05ac21f6cfa455d01c7cecddc58f3d5af
jongjin-j <jjjung3571@gmail.com>
2021-11-06 14:34:14 -0400

    Handling onChange events

--- monocle-frontend/src/App.js
+++ monocle-frontend/src/App.js
@@ -1,7 +1,12 @@
+import React, { useState } from 'react';
 import { TextField, Input, Button } from '@mui/material';
 import './App.css';
 
 function App() {
+  const [ URL, setURL ] = useState('');
+  const [ PDF, setPDF ] = useState('');
+
+  console.log(URL, PDF);
   return (
     <div>
         <div className="header">
@@ -13,18 +18,21 @@ function App() {
             label="URL" 
             variant="outlined" 
             placeholder="Enter a URL"
+            onChange={(e) => setURL(e.target.value)}
           />
         </div>
         <div className="or">
           OR
         </div>
         <div className="pdf-mode">
-        <label htmlFor="contained-button-file">
-          <Input accept="image/*" id="contained-button-file" multiple type="file" />
-          <Button variant="contained" component="span">
-            Upload
+          <label htmlFor="contained-button-file">
+            <Input accept="image/*" id="contained-button-file" multiple type="file" onChange={(e) => console.log(e.target.value)}/>
+          </label>
+        </div>
+        <div className="button">
+          <Button size="large" variant="contained" color="success" onClick={() => console.log('clicked')}>
+            Go
           </Button>
-        </label>
         </div>
     </div>
   );
