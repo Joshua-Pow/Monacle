a319802218de1b69c4351c08e6ea3a7edf89d892
TashanManiyalaghan <64664596+TashanManiyalaghan@users.noreply.github.com>
2021-11-06 17:03:09 -0400

    Added the getText function to the flask call for parsing a link.

--- monocle-backend/app.py
+++ monocle-backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from Helpers.linkParser import getText
 
 app = Flask(__name__)
 
 @app.route('/link')
 def parseLink(link):
-    return 0
+    text = getText(link)
 
 @app.route('/pdf/<PDF_Id>')
 def parsePDF(PDF_Id):
