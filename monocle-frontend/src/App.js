9f19e4d5d4d5d36b440df26e15ebefb72a8a9c00
Soraphoj(Peter) Damrongpiriyapong <peterdpong@gmail.com>
2021-11-06 17:45:10 -0400

    Added information and input changes

--- monocle-frontend/src/App.js
+++ monocle-frontend/src/App.js
@@ -32,7 +32,7 @@ function App() {
             <FormControlLabel value="PDF" control={<Radio />} label="PDF" />
           </RadioGroup>
         </FormControl>
-        <div>
+        <div className="input">
           {inputMethod === 'Link' ?
             <TextField 
               id="outlined-basic" 
@@ -49,7 +49,7 @@ function App() {
 
         </div>
         <div className="button">
-          <Button size="large" variant="contained" onClick={() => console.log('clicked')}>
+          <Button size="large" variant="contained" color="primary" onClick={() => console.log('clicked')}>
             Go
           </Button>
         </div>
@@ -60,18 +60,37 @@ function App() {
             <h1 className="whiteText">What is Monocle?</h1>
           </Grid>
           <Grid item xs={8}>
-            <h4 className="whiteText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam vulputate ut pharetra sit. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis. Arcu risus quis varius quam quisque id. Cras fermentum odio eu feugiat pretium nibh ipsum. Volutpat blandit aliquam etiam erat velit scelerisque in dictum non. Ultricies mi eget mauris pharetra et ultrices. Venenatis a condimentum vitae sapien. Lectus proin nibh nisl condimentum id venenatis a condimentum. Enim neque volutpat ac tincidunt vitae semper quis. Volutpat lacus laoreet non curabitur. Leo urna molestie at elementum eu facilisis sed odio morbi. A iaculis at erat pellentesque adipiscing commodo elit at. Facilisi nullam vehicula ipsum a arcu cursus.</h4>
+            <h4 className="whiteText">
+              Ever wonder what you agree to in those privacy policies you don't read? <br/>
+              Monocle reduces these lengthly documents down the the most important information about the safety and privacy of your data.
+              Simply link the privacy policy website, or upload a PDF and we will scan it and give you a summary of the most important parts of the policy that you should know.
+              We focus on showing information such as what data they collect, purpose of data being collected, how it is stored and affliated companies who have access to the data.
+            </h4>
           </Grid>
         </Grid>
 
-        <Grid container sx={{backgroundColor: '#6B4F4F', padding: '5px'}}>
+        <Grid container sx={{backgroundColor: '#6B4F4F', padding: '20px'}}>
           <Grid item xs={4}>
           <h1 className="whiteText">How does it work?</h1>
           </Grid>
           <Grid item xs={8}>
-            <h4 className="whiteText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam vulputate ut pharetra sit. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis. Arcu risus quis varius quam quisque id. Cras fermentum odio eu feugiat pretium nibh ipsum. Volutpat blandit aliquam etiam erat velit scelerisque in dictum non. Ultricies mi eget mauris pharetra et ultrices. Venenatis a condimentum vitae sapien. Lectus proin nibh nisl condimentum id venenatis a condimentum. Enim neque volutpat ac tincidunt vitae semper quis. Volutpat lacus laoreet non curabitur. Leo urna molestie at elementum eu facilisis sed odio morbi. A iaculis at erat pellentesque adipiscing commodo elit at. Facilisi nullam vehicula ipsum a arcu cursus.</h4>
+            <h4 className="whiteText">
+              For Monocle, we have built an algorithm to parse the text of the policy privacy. 
+              The algorithm looks for specific keywords and pulls out the sentence based on if we think it is important enough for the user.
+              To get the text, if a link is provided, we scrap the website and pull the text. If a PDF is provided, we parse the pdf file for the text.
+              Once we get the text, we run all the text through our algorithm to determine the key parts of the policy.
+            </h4>
           </Grid>
         </Grid>
+
+
+        <footer>
+          <Grid container alignItems="center" sx={{backgroundColor: '#292121', paddingX : '15px'}}>
+            <Grid item xs={12}>
+            <h4 className="whiteText">MONOCLE - NEWHACKS 2021</h4>
+            </Grid>
+          </Grid>
+        </footer>
     </div>
   );
 }
--- monocle-frontend/src/index.js
+++ monocle-frontend/src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#483434',
+      contrastText: '#fff',
+    }
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
