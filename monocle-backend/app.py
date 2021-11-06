4176a417bc9e8ba0b22e39261ccac1a05f3b86bc
racefn <jokesjosh@gmail.com>
2021-11-06 18:23:39 -0400

    Fixed PDF parsing of a URL for a PDF

--- monocle-backend/app.py
+++ monocle-backend/app.py
@@ -1,5 +1,8 @@
+from flask.json import jsonify
 import requests
-from flask import Flask
+from flask import Flask, request
+from Helpers.PDFParser import extract_text
+import PyPDF4
 
 app = Flask(__name__)
 
@@ -7,6 +10,16 @@ app = Flask(__name__)
 def parseLink():
     return 0
 
-@app.route('/pdf/<PDF_Id>')
-def parsePDF(PDF_Id):
-    return 0
+@app.route('/pdf/<PDFUrl>')
+def parsePDF(PDFUrl):
+    '''
+    param PDFUrl: URL of the PDF file to be parsed
+    '''
+    #PDFUrl = "https://firebasestorage.googleapis.com/v0/b/monocle-552fc.appspot.com/o/" + PDFUrl + "?alt=" + request.args.get("alt") + "&token=" + request.args.get("token")
+    PDFUrl = "http://www.africau.edu/images/default/sample.pdf"
+    r = requests.get(PDFUrl, stream=True)
+    if r.status_code == 200:
+        text = extract_text("temp.pdf")
+    else:
+        return "ERROR: Response " + str(r.status_code)
+    return jsonify(text)
