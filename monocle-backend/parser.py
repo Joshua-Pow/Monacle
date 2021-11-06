899b8c5c7ca243d4d9113a88656c9ac104c48ade
TashanManiyalaghan <64664596+TashanManiyalaghan@users.noreply.github.com>
2021-11-06 14:03:32 -0400

    Initialized Flask app.

---
+++ monocle-backend/parser.py
@@ -0,0 +1,13 @@
+import PyPDF2
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/link')
+def parseLink():
+    return 0
+
+@app.route('/pdf')
+def parsePDF():
+    return 0
