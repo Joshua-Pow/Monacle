c22df6e1c9dc58345aeb80e4084b67f8bb98eb27
racefn <jokesjosh@gmail.com>
2021-11-06 18:53:02 -0400

    Upadted requirments and returned in our app.py routes

--- monocle-backend/app.py
+++ monocle-backend/app.py
@@ -3,24 +3,25 @@ import requests
 from flask import Flask, request
 from Helpers.PDFParser import extract_text
 from Helpers.linkParser import getText
-import PyPDF4
 
 app = Flask(__name__)
 
 @app.route('/link')
 def parseLink(link):
     text = getText(link)
+    return jsonify(text)
 
 @app.route('/pdf/<PDFUrl>')
 def parsePDF(PDFUrl):
     '''
     param PDFUrl: URL of the PDF file to be parsed
     '''
-    #PDFUrl = "https://firebasestorage.googleapis.com/v0/b/monocle-552fc.appspot.com/o/" + PDFUrl + "?alt=" + request.args.get("alt") + "&token=" + request.args.get("token")
-    PDFUrl = "http://www.africau.edu/images/default/sample.pdf"
+    PDFUrl = "https://firebasestorage.googleapis.com/v0/b/monocle-552fc.appspot.com/o/" + PDFUrl + "?alt=" + request.args.get("alt") + "&token=" + request.args.get("token")
+    #PDFUrl = "http://www.africau.edu/images/default/sample.pdf"
     r = requests.get(PDFUrl, stream=True)
     if r.status_code == 200:
         text = extract_text("temp.pdf")
+        return jsonify(text)
     else:
         return "ERROR: Response " + str(r.status_code)
-    return jsonify(text)
+    
