997f8b408dedc7895ceef76a2c5cce6d2635b30e
TashanManiyalaghan <64664596+TashanManiyalaghan@users.noreply.github.com>
2021-11-06 16:58:55 -0400

    Parse a website and convert it into a string to easily parse it later.

---
+++ monocle-backend/Helpers/linkParser.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import requests
+
+def getText(link):
+    response = Request(url=link, headers={'User-Agent': 'Mozilla/5.0'})
+    html = urlopen(response).read()
+    soup = BeautifulSoup(html, features="html.parser")
+
+    for script in soup(["script", "style"]):
+        script.extract()
+
+    text = soup.get_text()
+
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    return text
--- monocle-backend/app.py
+++ monocle-backend/app.py
@@ -1,10 +1,9 @@
-import requests
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/link')
-def parseLink():
+def parseLink(link):
     return 0
 
 @app.route('/pdf/<PDF_Id>')
