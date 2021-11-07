adfa3ea2251a6a66d7a0e18d1d38bd077ca0d793
racefn <jokesjosh@gmail.com>
2021-11-07 01:12:02 -0500

    highlighted words in the privacy policy highlights

--- monocle-frontend/src/components/Result.js
+++ monocle-frontend/src/components/Result.js
@@ -4,7 +4,32 @@ import React from 'react';
 import './Result.css';
 
 export default function Result({ dataCollected, purposeOfData, highlights }) {
-  const testList = ['Name', 'Address', 'Payment'];
+  const testList = ['Name', 'Credit Card', 'Payment Method', 'Location', 'Age', 'Address', 'address', 'Picture', 'School', 'Website', 'Cookie', 'IP', 'Payment Information', 'Financial Data'];
+
+  //create a function to see if the word is in the testList array ignoring case and removing punctuation
+  const isInList = (word) => {
+    let wordNoPunctuation = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let wordNoPunctuationLower = wordNoPunctuation.toLowerCase();
+    let testListLower = testList.map(word => word.toLowerCase());
+    return testListLower.includes(wordNoPunctuationLower);
+  }
+
+
+//create a component to render each word in a string individually
+  const highlightKeywords = (string) => {
+    let words = string.split(' ');
+    //return each word in the words array
+    return words.map((word, index) => {
+      //if the word is in the testList array, set its background color to yellow
+      if (isInList(word)) {
+        return <span> <b key={index} style={{backgroundColor: 'yellow'}}>{word}</b></span>
+      } else {
+        return <span key={index}>{' '+word}</span>
+      }
+    })
+  }
+
+
 
   return (
       <Container sx={{marginBottom: '50px'}}>
@@ -35,7 +60,7 @@ export default function Result({ dataCollected, purposeOfData, highlights }) {
         <Card sx={{padding: '10px'}}>
           <List className="highlights">
             {highlights.map((highlight, index) => (
-              <ListItemText key={index}>{highlight}</ListItemText>
+              <ListItemText sx={{backgroundColor: (index%2) ? '#eeeeee' : '#FFFFFF'}} key={index}>{highlightKeywords(highlight)}</ListItemText>
             ))}
           </List>
         </Card>
